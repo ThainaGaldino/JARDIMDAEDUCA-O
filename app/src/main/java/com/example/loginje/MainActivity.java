7860d64cc4e90f131a66185b2ed4887c7eb13c0f
ThainaGaldino <Thainachrysostomo35@gmail.com>
2023-06-22 08:49:31 -0300

    primeiro commit Thaina

--- app/src/main/java/com/example/loginje/MainActivity.java
+++ app/src/main/java/com/example/loginje/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.loginje;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+<<<<<<< HEAD
 import android.annotation.SuppressLint;
+=======
+>>>>>>> b0d4214 (primeiro commit Thaina)
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -16,6 +19,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+<<<<<<< HEAD
 public class cadastro extends AppCompatActivity {}
 
         private TextView mTextViewserie, mTextViewcadastro ;
@@ -74,6 +78,86 @@ public class cadastro extends AppCompatActivity {}
         mButtoncadastro=findViewById(R.id.button_cadastro);
         mButtoncadastro.setOnClickListener(new ClickButtonEntrar());
 
+=======
+
+    private EditText mEditextCredentials , mEdittextPassword ;
+    private TextView mButtonSignUp;
+    private Button mButtonSignIn  ;
+
+
+    private boolean isRequired(){
+        if (TextUtils.isEmpty(mEditextCredentials.getText()) ||
+        TextUtils.isEmpty(mEdittextPassword.getText())) {
+            return true;
+
+        } else {
+            return false;
+        }
+    }
+
+
+    private void performNextActivity (){
+        if (isRequired()){
+            Toast.makeText(this,"Mandatory information", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Toast.makeText(this,"trocar de janela",Toast.LENGTH_SHORT).show();
+
+     //trocar de janela (tela de pres) //
+
+    }
+    private void performNextActivity2 (){
+
+        Toast.makeText(this,"Realizando Cadastro",Toast.LENGTH_SHORT).show();
+
+        //trocar de janela (tela de pres) //
+        Intent it= new Intent(MainActivity.this , RedefinirActivity.class);
+        startActivity(it);
+
+    }
+    public class ClickButtonSiggnIn implements View
+    .OnClickListener{
+        @Override
+        public void onClick(View view) {
+            performNextActivity();
+        }
+    }
+    public class ClickButtonSiggnUp implements View
+            .OnClickListener{
+        @Override
+        public void onClick(View view) {
+            performNextActivity2();
+        }
+    }
+
+   public class EditTextAction implements TextView.OnEditorActionListener{
+       @Override
+       public boolean onEditorAction(TextView textView, int actionID, KeyEvent keyEvent) {
+
+           if(actionID == EditorInfo.IME_ACTION_DONE){
+               performNextActivity();
+           }
+
+
+           return false;
+       }
+   }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mEditextCredentials = findViewById(R.id.editText_credentials) ;
+        mEdittextPassword = findViewById(R.id.editText_password);
+        mEdittextPassword.setOnEditorActionListener(new EditTextAction());
+        mButtonSignIn = findViewById(R.id.btn_sign_in);
+        mButtonSignIn.setOnClickListener(new ClickButtonSiggnIn());
+
+        mButtonSignUp = findViewById(R.id.btn_forgot_password);
+        mButtonSignUp.setOnClickListener(new ClickButtonSiggnUp());
+>>>>>>> b0d4214 (primeiro commit Thaina)
 
     }
 }
---
+++ app/src/main/java/com/example/loginje/RedefinirActivity.java
@@ -0,0 +1,20 @@
+package com.example.loginje;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RedefinirActivity  extends AppCompatActivity {
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_redefinir);
+
+
+
+    }
+}
