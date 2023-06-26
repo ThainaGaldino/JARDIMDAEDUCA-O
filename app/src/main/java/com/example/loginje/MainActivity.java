449188183248d03db8dcba3e362cd18e7e538a1b
ThainaGaldino <Thainachrysostomo35@gmail.com>
2023-06-26 12:26:56 -0300

    Alterações Thaina 26-06

--- app/src/main/java/com/example/loginje/MainActivity.java
+++ app/src/main/java/com/example/loginje/MainActivity.java
@@ -2,10 +2,9 @@ package com.example.loginje;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-<<<<<<< HEAD
+
 import android.annotation.SuppressLint;
-=======
->>>>>>> b0d4214 (primeiro commit Thaina)
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -18,67 +17,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-<<<<<<< HEAD
-public class cadastro extends AppCompatActivity {}
-
-        private TextView mTextViewserie, mTextViewcadastro ;
-        private EditText mEditTextdatadenascimento, mEditTextemaildoresponsavel, mEditTextnomealuno ;
-        private Button mButtoncadastro;
-
-        //campo é obrigatório e não pode ser deixado em branco ou vazio
-        private boolean isRequired(){
-            if(TextUtils.isEmpty(mEditTextnomealuno.getText()) ||
-                    TextUtils.isEmpty(mEditTextdatadenascimento.getText()) ||
-                    TextUtils.isEmpty(mEditTextemaildoresponsavel.getText()) ) {
-                return true;
-            } else {}
-            return false;
-        }
-        //responsável por iniciar ou executar a próxima atividade (activity) no fluxo de um aplicativo Android.
-        private void performNextActivity(){
-            if(isRequired()){
-                Toast.makeText(this,"Mandatory information", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(MainActivity.this, presactivity.class));
-            }
-        }
-
-        public static class ClickButtonEntrar implements View
-
-        .OnClickListener{
-            @Override
-            public void onClick(View v) {
-
-            }
-        }
-
-            public class EditTextAction implements TextView.OnEditorActionListener{
-                @Override
-                public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-
-                    if (actionId == EditorInfo.IME_ACTION_DONE){
-                        performNextActivity();
-                    }
-
-
-                    return false;
-                }
-            }
-
-    @SuppressLint("MissingInflatedId")
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cadastro);
-
-        mEditTextnomealuno=findViewById(R.id.editText_nome_aluno);
-        mEditTextdatadenascimento=findViewById(R.id.editText_data_de_nascimento);
-        mEditTextemaildoresponsavel=findViewById(R.id.editText_email_do_responsavel);
-
-        mButtoncadastro=findViewById(R.id.button_cadastro);
-        mButtoncadastro.setOnClickListener(new ClickButtonEntrar());
-
-=======
 
     private EditText mEditextCredentials , mEdittextPassword ;
     private TextView mButtonSignUp;
@@ -157,7 +95,13 @@ public class cadastro extends AppCompatActivity {}
 
         mButtonSignUp = findViewById(R.id.btn_forgot_password);
         mButtonSignUp.setOnClickListener(new ClickButtonSiggnUp());
->>>>>>> b0d4214 (primeiro commit Thaina)
 
+    }
+
+    public static class ClickButtonConcluir implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+
+        }
     }
 }
--- app/src/main/java/com/example/loginje/cadastro.java
+++ app/src/main/java/com/example/loginje/cadastro.java
@@ -16,48 +16,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class cadastro extends AppCompatActivity {
 
-    private TextView mTextViewserie, mTextViewcadastro ;
-    private EditText mEditTextdatadenascimento, mEditTextemaildoresponsavel, mEditTextnomealuno ;
-    private Button mButtoncadastro;
-
-    //campo é obrigatório e não pode ser deixado em branco ou vazio
-    private boolean isRequired(){
-        if(TextUtils.isEmpty(mEditTextnomealuno.getText()) ||
-                TextUtils.isEmpty(mEditTextdatadenascimento.getText()) ||
-                TextUtils.isEmpty(mEditTextemaildoresponsavel.getText()) ) {
-            return true;
-        } else {}
-        return false;
-    }
-    //responsável por iniciar ou executar a próxima atividade (activity) no fluxo de um aplicativo Android.
-    private void performNextActivity(){
-        if(isRequired()){
-            Toast.makeText(this,"Mandatory information", Toast.LENGTH_SHORT).show();
-            startActivity(new Intent(cadastro.this, presactivity.class));
-        }
-    }
-
-public class ClickButtonEntrar implements View
-
-        .OnClickListener{
-    @Override
-    public void onClick(View v) {
-
-    }
-}
-
-public class EditTextAction implements TextView.OnEditorActionListener{
-    @Override
-    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-
-        if (actionId == EditorInfo.IME_ACTION_DONE){
-            performNextActivity();
-        }
-
-
-        return false;
-    }
-}
+    private TextView mTextViewserie, mTextViewcadastro;
+    private EditText mEditTextdatadenascimento, mEditTextemaildoresponsavel, mEditTextnomealuno;
+    private Button mButtonconcluir;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -65,14 +26,56 @@ public class EditTextAction implements TextView.OnEditorActionListener{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
 
-        mEditTextnomealuno=findViewById(R.id.editText_nome_aluno);
-        mEditTextdatadenascimento=findViewById(R.id.editText_data_de_nascimento);
-        mEditTextemaildoresponsavel=findViewById(R.id.editText_email_do_responsavel);
+        mEditTextnomealuno = findViewById(R.id.editText_nome_aluno);
+        mEditTextdatadenascimento = findViewById(R.id.editText_data_de_nascimento);
+        mEditTextemaildoresponsavel = findViewById(R.id.editText_email_do_responsavel);
 
-        mButtoncadastro=findViewById(R.id.button_cadastro);
-        mButtoncadastro.setOnClickListener(new MainActivity.ClickButtonEntrar());
+        mButtonconcluir = findViewById(R.id.button_concluir);
+        mButtonconcluir.setOnClickListener(new MainActivity.ClickButtonConcluir());
+
+
+    }
+
+    //campo é obrigatório e não pode ser deixado em branco ou vazio
+    private boolean isRequired() {
+        if (TextUtils.isEmpty(mEditTextnomealuno.getText()) ||
+                TextUtils.isEmpty(mEditTextdatadenascimento.getText()) ||
+                TextUtils.isEmpty(mEditTextemaildoresponsavel.getText())) {
+            return true;
+        } else {
+        }
+        return false;
+    }
+
+    //responsável por iniciar ou executar a próxima atividade (activity) no fluxo de um aplicativo Android.
+    private void performNextActivity() {
+        if (isRequired()) {
+            Toast.makeText(this, "Mandatory information", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(cadastro.this, MainActivity.class));
+        }
+    }
+
+    public class ClickButtonConcluir implements View
+
+            .OnClickListener {
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+
+    public class EditTextAction implements TextView.OnEditorActionListener {
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                performNextActivity();
+            }
+
+
+            return false;
+        }
 
 
     }
 }
-
