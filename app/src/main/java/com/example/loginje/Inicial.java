dace32f9f9edf36b5dc41a26ae0fe4d0ff5c3cde
SarahIngrid <contato.sarahicgomes@gmail.com>
2023-06-28 09:08:49 -0300

    alteração Sarah

--- app/src/main/java/com/example/loginje/Inicial.java
+++ app/src/main/java/com/example/loginje/Inicial.java
@@ -1,7 +1,68 @@
 package com.example.loginje;
 
-public class Inicial {
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Inicial extends AppCompatActivity {
+
+    private TextView mTextViewinicial;
+    private Button mButtoncadastrar, mButtonentrar;
+    private boolean required;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicial);
+
+
+        mButtoncadastrar = findViewById(R.id.button_cadastrar);
+        mButtoncadastrar.setOnClickListener(new ClickButtonCadastrar());
+
+        mButtonentrar = findViewById(R.id.button_entrar);
+        mButtonentrar.setOnClickListener(new ClickButtonEntrar());
 
 
 
+
+        //responsável por iniciar ou executar a próxima atividade (activity) no fluxo de um aplicativo Android.
+
+        class ClickButtonCadastrar implements View.OnClickListener {
+            @Override
+            public void onClick(View v) {
+
+            }
+        }
+
+}
+
+    public boolean isRequired() {
+        return required;
+    }
+
+    public void setRequired(boolean required) {
+        this.required = required;
+    }
+
+    public class ClickButtonCadastrar implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+
+    public class ClickButtonEntrar implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
 }
