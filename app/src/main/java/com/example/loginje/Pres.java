c36eb2ecdf6e0807f1467a4eece9db35aeca8ec1
SarahIngrid <contato.sarahicgomes@gmail.com>
2023-07-04 20:15:50 -0300

    Atualização

--- app/src/main/java/com/example/loginje/Pres.java
+++ app/src/main/java/com/example/loginje/Pres.java
@@ -1,16 +1,32 @@
 package com.example.loginje;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Pres  extends AppCompatActivity {
+
+    private TextView mTextViewpre1, mTextViewpre2, mTextViewpre3;
+
     @Override
 protected void onPostCreate(@Nullable Bundle savedInstanceState) {
     super.onPostCreate(savedInstanceState);
 
     setContentView(R.layout.activity_pres);
+
+        class TextViewpre1 implements View.OnClickListener {
+            @Override
+            public void onClick(View view) {
+                //trocar de janela (tela de cadastro) //
+                Intent it= new Intent(Pres.this, Habilidade.class);
+                startActivity(it);
+            }
+        }
+
 
 
 
