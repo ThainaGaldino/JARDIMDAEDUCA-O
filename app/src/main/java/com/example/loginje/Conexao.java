3274d8bdb235f518c34a222cc7ce59ee7d60ee31
SarahIngrid <contato.sarahicgomes@gmail.com>
2023-08-10 08:11:51 -0300

    Atualização Sarah

---
+++ app/src/main/java/com/example/loginje/Conexao.java
@@ -0,0 +1,30 @@
+package com.example.loginje;
+
+import android.os.StrictMode;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexao {
+     public Connection conectar  () {
+          Connection conn = null;
+
+          try {
+               StrictMode.ThreadPolicy policy;
+               policy = new StrictMode.ThreadPolicy.Builder()
+                       .permitAll() .build();
+               StrictMode.setThreadPolicy(policy);
+               Class.forName("net.sourceforge.jtds.jsbc.Driver").newInstance();
+               conn = DriverManager.getConnection("jdbc:jtds:sqlserver://172.19.1.94;" +
+                       "databaseName= bd_jardimdaeducacao ; user=sa; password+123");
+
+
+
+          } catch (Exception e) {
+               e.printStackTrace();
+          }
+
+          return null
+
+     }
+}
